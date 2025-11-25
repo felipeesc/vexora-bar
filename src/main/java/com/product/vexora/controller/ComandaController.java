2600108d7d218f635c25539dec4d8d4d3861aca1
Felipe Carvalho <felipe.carvalho@postalis.com.br>
2025-11-24 23:39:13 -0300

    [FEAT] incluindo  @PreAuthorize("isAuthenticated()") na ComandaController

--- src/main/java/com/product/vexora/controller/ComandaController.java
+++ src/main/java/com/product/vexora/controller/ComandaController.java
@@ -5,6 +5,7 @@ import com.product.vexora.dto.ComandaRequestDTO;
 import com.product.vexora.dto.ComandaResponseDTO;
 import com.product.vexora.service.ComandaService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
@@ -17,16 +18,19 @@ public class ComandaController {
     private final ComandaService service;
 
     @PostMapping("/abrir")
+    @PreAuthorize("isAuthenticated()")
     public ComandaResponseDTO abrir(@RequestBody ComandaRequestDTO dto) {
         return service.abrirComanda(dto);
     }
 
     @PostMapping("/item")
+    @PreAuthorize("isAuthenticated()")
     public ComandaResponseDTO adicionarItem(@RequestBody ComandaItemRequestDTO dto) {
         return service.adicionarItem(dto);
     }
 
     @PostMapping("/{id}/fechar")
+    @PreAuthorize("isAuthenticated()")
     public ComandaResponseDTO fechar(@PathVariable UUID id) {
         return service.fecharComanda(id);
     }
