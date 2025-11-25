ce32ef06f739d0f8fbd378f03399acb715d7e0e9
Felipe Carvalho <felipe.carvalho@postalis.com.br>
2025-11-25 19:02:30 -0300

    [FIX] Corrigindo imports do jakarta.ID

--- src/main/java/com/product/vexora/entity/Funcionario.java
+++ src/main/java/com/product/vexora/entity/Funcionario.java
@@ -3,11 +3,11 @@ package com.product.vexora.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
 
 import java.util.UUID;
 
@@ -21,6 +21,7 @@ public class Funcionario {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
+
     private String nome;
     private Double porcentagem;
 }
